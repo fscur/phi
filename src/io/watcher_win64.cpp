8e23b8af8922c526655164fe9c8a3d84f5a298ca
Thiago Cândido <Thiago Cndido>
2016-05-19 21:54:18 +0100

    fixing gcc error to CancelIoEx(windows.h) call
    Former-commit-id: c202ebfee7de8be1f77ba5a2dcf7a715cf84c8ee
    Former-commit-id: 90d00fe295932821a5eaad4f538432b426a78d9c

--- src/io/watcher_win64.cpp
+++ src/io/watcher_win64.cpp
@@ -1,7 +1,7 @@
 #include <precompiled.h>
-#include <WinBase.h>
-#include <Windows.h>
+
 #include "watcher.h"
+#include <diagnostics\windowsProcLoader.h>
 
 namespace phi
 {
@@ -89,7 +89,7 @@ namespace phi
         {
             _programRunning = false;
 
-            if (CancelIoEx(_hDir, NULL))
+            if (windowsProcLoader::cancelIoEx(_hDir, NULL))
             {
                 _watcherThread.join();
             }
