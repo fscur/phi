debeff2f9870a277775e314756d608e521b872dc
Patrick dos Santos <patrickds@outlook.com>
2016-05-24 15:43:46 -0300

    Removed tabs
    Former-commit-id: 094dc72bac29f5a73fd3d2eefceb16afa9c935f3
    Former-commit-id: 6e5e3f670ba909e522c92c8c03bfb904bc791c64

--- src/demon/main.cpp
+++ src/demon/main.cpp
@@ -18,26 +18,26 @@ using namespace phi;
 
 extern "C"
 {
-	API unsigned long NvOptimusEnablement = 1;
-	API int AmdPowerXpressRequestHighPerformance = 1;
+    API unsigned long NvOptimusEnablement = 1;
+    API int AmdPowerXpressRequestHighPerformance = 1;
 }
 
 struct commandLineCommand
 {
 public:
-	string name;
-	vector<string> args;
-	void(*func)(vector<string>);
-	bool shouldExecute;
+    string name;
+    vector<string> args;
+    void(*func)(vector<string>);
+    bool shouldExecute;
 
 public:
-	commandLineCommand(string name, void(*func)(vector<string>)) :
-		name(name),
-		args(vector<string>()),
-		func(func),
-		shouldExecute(false)
-	{
-	}
+    commandLineCommand(string name, void(*func)(vector<string>)) :
+        name(name),
+        args(vector<string>()),
+        func(func),
+        shouldExecute(false)
+    {
+    }
 };
 
 vector<commandLineCommand> commandLineCommands;
@@ -46,102 +46,102 @@ string _libraryPath;
 
 void rpCommandFunction(vector<string> args)
 {
-	_resourcesPath = args[0];
+    _resourcesPath = args[0];
 }
 
 void lpCommandFunction(vector<string> args)
 {
-	_libraryPath = args[0];
+    _libraryPath = args[0];
 }
 
 void initializeCommandLineCommands()
 {
-	/*
-	/rp <path> resources path
-	/lp <path> library path
-	*/
+    /*
+    /rp <path> resources path
+    /lp <path> library path
+    */
 
-	commandLineCommands.push_back(commandLineCommand("/rp", &rpCommandFunction));
-	commandLineCommands.push_back(commandLineCommand("/lp", &lpCommandFunction));
+    commandLineCommands.push_back(commandLineCommand("/rp", &rpCommandFunction));
+    commandLineCommands.push_back(commandLineCommand("/lp", &lpCommandFunction));
 }
 
 void processCommandLine(int argc, char* args[])
 {
-	size_t lastCommand = 0;
-	auto foundLastCommand = false;
+    size_t lastCommand = 0;
+    auto foundLastCommand = false;
 
-	for (int i = 1; i < argc; i++)
-	{
-		string arg(args[i]);
+    for (int i = 1; i < argc; i++)
+    {
+        string arg(args[i]);
 
-		bool foundCommand = false;
+        bool foundCommand = false;
 
-		for (size_t cmdIndex = 0; cmdIndex < commandLineCommands.size(); cmdIndex++)
-		{
-			if (arg.compare(commandLineCommands[cmdIndex].name) == 0)
-			{
-				commandLineCommands[cmdIndex].shouldExecute = true;
-				lastCommand = cmdIndex;
-				foundLastCommand = true;
-				foundCommand = true;
-				break;
-			}
-		}
+        for (size_t cmdIndex = 0; cmdIndex < commandLineCommands.size(); cmdIndex++)
+        {
+            if (arg.compare(commandLineCommands[cmdIndex].name) == 0)
+            {
+                commandLineCommands[cmdIndex].shouldExecute = true;
+                lastCommand = cmdIndex;
+                foundLastCommand = true;
+                foundCommand = true;
+                break;
+            }
+        }
 
-		if (!foundCommand && foundLastCommand)
-			commandLineCommands[lastCommand].args.push_back(arg);
-	}
+        if (!foundCommand && foundLastCommand)
+            commandLineCommands[lastCommand].args.push_back(arg);
+    }
 
-	return;
+    return;
 }
 
 void executeCommands()
 {
-	auto count = commandLineCommands.size();
+    auto count = commandLineCommands.size();
 
-	for (unsigned int i = 0; i < count; i++)
-		if (commandLineCommands[i].shouldExecute)
-			commandLineCommands[i].func(commandLineCommands[i].args);
+    for (unsigned int i = 0; i < count; i++)
+        if (commandLineCommands[i].shouldExecute)
+            commandLineCommands[i].func(commandLineCommands[i].args);
 }
 
 void initializeGetText()
 {
-	setlocale(LC_ALL, "");
-	bindtextdomain("demon", "./locale");
-	textdomain("demon");
+    setlocale(LC_ALL, "");
+    bindtextdomain("demon", "./locale");
+    textdomain("demon");
 }
 
 void initializeDiagnostics(const char* executableFileName)
 {
-	phi::platformProcLoader::load(executableFileName);
+    phi::platformProcLoader::load(executableFileName);
 }
 
 int main(int argc, char* args[])
 {
-	char executableFileName[FILENAME_MAX];
-	strcpy_s(executableFileName, args[0]);
+    char executableFileName[FILENAME_MAX];
+    strcpy_s(executableFileName, args[0]);
 
-	initializeDiagnostics(executableFileName);
-	initializeGetText();
-	initializeCommandLineCommands();
-	processCommandLine(argc, args);
-	executeCommands();
+    initializeDiagnostics(executableFileName);
+    initializeGetText();
+    initializeCommandLineCommands();
+    processCommandLine(argc, args);
+    executeCommands();
 
-	try
-	{
-		auto appStartInfo = applicationStartInfo(executableFileName, _resourcesPath, "");
-		auto application = phi::application(appStartInfo);
-		auto screen = demon::screen("?", 1920, 1080);
+    try
+    {
+        auto appStartInfo = applicationStartInfo(executableFileName, _resourcesPath, "");
+        auto application = phi::application(appStartInfo);
+        auto screen = demon::screen("?", 1920, 1080);
 
-		application.run(&screen);
-	}
-	catch (const phi::invalidInitializationException& exception)
-	{
-		application::logError(exception.what());
-		system("pause");
-	}
+        application.run(&screen);
+    }
+    catch (const phi::invalidInitializationException& exception)
+    {
+        application::logError(exception.what());
+        system("pause");
+    }
 
-	phi::input::release();
+    phi::input::release();
 
-	return 0;
+    return 0;
 }
